919f7d9ca177b0743ca11eb6d84b7b0a1db7f6d6
Cezarion <cezarion@cezarion.net>
2015-10-19 07:18:38 +0200

    Update 30_osx_homebrew_recipes.sh
    Add php

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -4,6 +4,15 @@ is_osx || return 1
 # Exit if Homebrew is not installed.
 [[ ! "$(type -P brew)" ]] && e_error "Brew recipes need Homebrew to install." && return 1
 
+# Homebrew tap
+kegs=(
+      homebrew/dupes
+      homebrew/versions
+      homebrew/homebrew-php
+    )
+brew_tap_kegs
+
+
 # Homebrew recipes
 recipes=(
   android-platform-tools
